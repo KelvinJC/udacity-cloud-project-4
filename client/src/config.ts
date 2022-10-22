83a8f2a61a53b3592510b2668308d5602a0faead
Kelvin JC <ojkelvinchu@gmail.com>
2022-10-22 10:42:34 +0100

    project submission

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '44ww3fpa47';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-x4wb6zhngqgmh0kf.us.auth0.com',
+  clientId: 'PqErSrwHVrGWHRm7p96jrWqn2G90c5DF',
+  callbackUrl: 'http://localhost:3000/callback'
+};
